e4d46f9cbf938fbd6ee1198aed141e75605abceb
paul cannon <paul@riptano.com>
2010-11-18 13:29:32 -0700

    first pass at basic packaging

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(morbid):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="morbid" command="/usr/bin/morbid"
--- debian/morbid.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the morbid package
-#
-0 4	* * *	root	[ -x /usr/bin/morbid_maintenance ] && /usr/bin/morbid_maintenance
